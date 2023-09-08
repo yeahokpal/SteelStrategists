890367c42a17c0a8ed6478d2e06a7f51aeb3f013
Jack Gill <jaganimations@gmail.com>
2023-09-08 14:09:55 -0400

    Work on SaveManager

--- Assets/SQLite/SaveManager.cs
+++ Assets/SQLite/SaveManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mono.Data.Sqlite;
 
 public class SaveManager : MonoBehaviour
 {
+    string dbName = "URI=file:Database.db";
+
     // Start is called before the first frame update
     void Start()
     {
