2a6f98272cdcd4607b1d4320ff1fd5e99bb9a54f
yeahokpal <113112958+yeahokpal@users.noreply.github.com>
2023-09-18 14:02:06 -0400

    Started Dialog System

---
+++ Assets/DialogueBox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogBox : MonoBehaviour
+{
+    public DialogBox(string characterName, string text)
+    {
+
+    }
+
+    public DialogBox(string characterName, string text, Sprite characterPortrait)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
