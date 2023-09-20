2b856ee8884f072a496ccb39d6a66a0c73557448
Jack Gill <jaganimations@gmail.com>
2023-09-20 13:45:04 -0400

    Audio Control

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -9,16 +11,20 @@ public class GameManager : MonoBehaviour
     float timerLengthSeconds = 120;
     bool startTimer = true;
     [SerializeField] TextMeshProUGUI timerTxt;
+    [SerializeField] Slider volumeSlider;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Put in a method later
+        AudioListener.volume = volumeSlider.value;
+
         if (startTimer)
         {
             timerTxt.text = (int)(timerLengthSeconds - (int)Time.realtimeSinceStartup + timerDelay) / 60 + ":" + (timerLengthSeconds - (int)Time.realtimeSinceStartup + timerDelay) % 60;
